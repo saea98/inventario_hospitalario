9e4674fb54af3c342d2c50e8b52c09e93389a511
Admin <admin@inventario.local>
2025-12-18 19:49:26 -0500

    Feat: Agregar Fase 2.5 - Reportes y Análisis Avanzados de Devoluciones con gráficos interactivos

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 #from inventario.admin import inventario_admin
-from inventario import urls_entrada_salida, urls_fase2, urls_inventario, urls_devoluciones
+from inventario import urls_entrada_salida, urls_fase2, urls_inventario, urls_devoluciones, urls_reportes_devoluciones
 from django.contrib import admin as django_admin
 
 
@@ -81,6 +81,7 @@ urlpatterns = [
     path('logistica/', include(urls_fase2)),  # URLs de FASE 2: Citas, Traslados, Conteo
     path('gestion-inventario/', include(urls_inventario)),  # URLs de FASE 2.3: Gestión de Inventario
     path('devoluciones/', include(urls_devoluciones)),  # URLs de FASE 2.4: Devoluciones de Proveedores
+    path('reportes/devoluciones/', include(urls_reportes_devoluciones)),  # URLs de FASE 2.5: Reportes de Devoluciones
     path('admin/', django_admin.site.urls),
     #path("select2/", include("django_select2.urls")),
     
