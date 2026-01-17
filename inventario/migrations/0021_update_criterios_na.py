a2ab2fadd15f866200ba1621782ac18d026bafca
saea98 <saea98@gmail.com>
2026-01-17 18:32:12 -0500

    Migration: Actualizar criterios 5 y 6 existentes a N/A

---
+++ inventario/migrations/0021_update_criterios_na.py
@@ -0,0 +1,27 @@
+# Generated migration to update criteria 5 and 6 to N/A
+
+from django.db import migrations
+
+def update_criterios_na(apps, schema_editor):
+    """Actualizar criterios 5 y 6 a N/A"""
+    ItemRevision = apps.get_model('inventario', 'ItemRevision')
+    
+    # Actualizar criterios 5 y 6 a N/A
+    ItemRevision.objects.filter(orden__in=[5, 6]).update(resultado='na')
+
+def reverse_update(apps, schema_editor):
+    """Revertir cambios"""
+    ItemRevision = apps.get_model('inventario', 'ItemRevision')
+    
+    # Revertir a SI
+    ItemRevision.objects.filter(orden__in=[5, 6]).update(resultado='si')
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventario', '0020_citaproveedor_tipo_entrega'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_criterios_na, reverse_update),
+    ]
