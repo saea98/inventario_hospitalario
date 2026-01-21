2a43bac2b5dd2d8ac2452bce39e8c3544a298f05
Admin <admin@inventario.local>
2026-01-21 16:38:23 -0500

    Fix: Agregar namespace 'reportes' a reportes_urls.py y corregir referencias en template

--- inventario/reportes_urls.py
+++ inventario/reportes_urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .reportes_views import ReporteEntradasView
 from .views_reporte_disponibilidad import reporte_disponibilidad_lotes, exportar_disponibilidad_excel
 
+app_name = 'reportes'
+
 urlpatterns = [
     path("entradas/", ReporteEntradasView.as_view(), name="reporte_entradas"),
     path("disponibilidad-lotes/", reporte_disponibilidad_lotes, name="reporte_disponibilidad_lotes"),
