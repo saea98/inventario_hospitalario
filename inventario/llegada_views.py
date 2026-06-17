a30446a79e03a880facc381386b9102ae1b16dd2
angel <angel.cl2010@hotmail.com>
2026-06-17 12:00:27 -0600

    Corrección de imagenes de EPA y correccion de excel llegadas (cambio de texto)

--- inventario/llegada_views.py
+++ inventario/llegada_views.py
@@ -1663,7 +1663,7 @@ def api_cita_folio(request, cita_id):
 
 # Firmas del EPA: nombres fijos (ajustar aquí cuando cambien los titulares)
 EPA_ENCARGADA_AREA_ENTRADAS = 'Mtra. Lluvia Marlene Barona Franco'
-EPA_ENCARGADA_OFICINA_INSUMOS = 'Lic. Elizabeth Acosta Escobar'
+EPA_ENCARGADA_OFICINA_INSUMOS = 'Rashab Rubio García'
 
 
 class ImprimirEPAView(LoginRequiredMixin, View):
